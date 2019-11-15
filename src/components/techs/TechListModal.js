b4f1084cfdd22f1e9a475b21d40cccc3c10b8a99
yash.garg <yash.garg@adpushup.com>
2019-11-15 13:03:46 +0530

    [Feature] (Frontend: App) Ops Logger

---
+++ src/components/techs/TechListModal.js
@@ -0,0 +1,39 @@
+import React, { useEffect } from "react";
+import TechItem from "./TechItem";
+import { connect } from "react-redux";
+import { getTechs } from "../../actions/techActions";
+import PropTypes from "prop-types";
+
+const TechListModal = ({ getTechs, tech: { techs, loading } }) => {
+  useEffect(() => {
+    getTechs();
+    //eslint-disable-next-line
+  }, []);
+
+  return (
+    <div id='tech-list-modal' className='modal'>
+      <div className='modal-content'>
+        <h4>Technician List</h4>
+        <ul className='collection'>
+          {!loading &&
+            techs !== null &&
+            techs.map(tech => <TechItem tech={tech} key={tech.id} />)}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+TechListModal.propTypes = {
+  tech: PropTypes.object.isRequired,
+  getTechs: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  tech: state.tech
+});
+
+export default connect(
+  mapStateToProps,
+  { getTechs }
+)(TechListModal);
